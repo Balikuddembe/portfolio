657927d4def5208e16841ff9156122f7edda188b
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-05-30 19:22:47 +0300

    Add tailwindcss to the elemnts

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import Image from 'next/image'
+import HomePage from '@/components/HomePage'
 import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
-      <div className=' bg-blue-500 font-bold'>Home page</div>
+      <HomePage />
     </main>
   )
 }
