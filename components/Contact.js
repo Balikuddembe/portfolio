402aeb30447f2e96fe8ee28b26eaa915c5568a3e
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-06-18 22:03:58 +0300

    Implement contact page

--- components/Contact.js
+++ components/Contact.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { AiOutlinePhone, AiOutlineMail } from 'react-icons/ai'
+import { MdLocationOn } from 'react-icons/md'
+
 
 const Contact = () => {
     return (
@@ -10,11 +13,29 @@ const Contact = () => {
                 </div>
                 <div className=' mt-8'>
                     <h2 className='text-2xl font-bold text-white'>Get in Touch</h2>
-                    <p className='mt-4 max-w-xl text-slate-400'>
+                    <p className='mt-4 max-w-sm text-slate-400'>
                         If you have an exciting application you're eager to develop, a specific feature 
                         that requires skilled implementation, or a project that demands coding expertise,
                         I would be delighted to offer my assistance and contribute to its success.
                     </p>
+                    <div>
+                        <div>
+                            <div>
+                                <AiOutlinePhone size={25} className=' text-rose-700'/>
+                            </div>
+                            <p className='text-slate-400'>+256 777 923 074</p>
+
+                            <div>
+                                <AiOutlineMail size={25} className=' text-rose-700'/>
+                            </div>
+                            <p className='text-slate-400'>+256 777 923 074</p>
+
+                            <div>
+                                <MdLocationOn size={25} className=' text-rose-700' />
+                            </div>
+                            <p className='text-slate-400'>Uganda</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
