405ab8ba1ea44c7639c55dcb7d72534662f82e30
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-06-19 14:05:43 +0300

    Add tailwind style to the contact page

--- components/Contact.js
+++ components/Contact.js
@@ -5,37 +5,50 @@ import { MdLocationOn } from 'react-icons/md'
 
 const Contact = () => {
     return (
-        <section className=' bg-black'>
+        <section className=' bg-black pb-40'>
             <div className=' ml-72'>
                 <div className=' text-white pt-24 flex font-semibold space-x-2'>
                     <span className="text-rose-700 text-3xl">|</span>
                     <h2 className='text-3xl'>Contact</h2>
                 </div>
-                <div className=' mt-8'>
-                    <h2 className='text-2xl font-bold text-white'>Get in Touch</h2>
-                    <p className='mt-4 max-w-sm text-slate-400'>
-                        If you have an exciting application you're eager to develop, a specific feature 
-                        that requires skilled implementation, or a project that demands coding expertise,
-                        I would be delighted to offer my assistance and contribute to its success.
-                    </p>
-                    <div>
-                        <div>
-                            <div>
-                                <AiOutlinePhone size={25} className=' text-rose-700'/>
-                            </div>
-                            <p className='text-slate-400'>+256 777 923 074</p>
+                <div className=' mt-8 flex'>
+                    <div className=' bg-slate-900 p-7 w-1/2'>
+                        <h2 className='text-2xl font-bold text-white'>Get in Touch</h2>
+                        <p className='mt-4 max-w-sm text-slate-400'>
+                            If you have an exciting application you're eager to develop, a specific feature
+                            that requires skilled implementation, or a project that demands coding expertise,
+                            I would be delighted to offer my assistance and contribute to its success.
+                        </p>
 
-                            <div>
-                                <AiOutlineMail size={25} className=' text-rose-700'/>
+                        <div className='flex space-x-4 mt-6 items-center'>
+                            <div className=' flex items-center justify-center bg-rose-700 w-9 h-9'>
+                                <AiOutlinePhone size={25} className=' text-white' />
                             </div>
-                            <p className='text-slate-400'>+256 777 923 074</p>
+                            <p className='text-slate-400'>+256 (0) 777-923-074</p>
+                        </div>
 
-                            <div>
-                                <MdLocationOn size={25} className=' text-rose-700' />
+                        <div className=' flex space-x-4 mt-3 items-center'>
+                            <div className=' flex items-center justify-center bg-rose-700 w-9 h-9'>
+                                <AiOutlineMail size={25} className=' text-white' />
                             </div>
-                            <p className='text-slate-400'>Uganda</p>
+                            <p className='text-slate-400'>balixemma@gmail.com</p>
+                        </div>
+
+                        <div className='flex space-x-4 mt-3 items-center'>
+                            <div className=' flex items-center justify-center w-9 h-9 bg-rose-700'>
+                                <MdLocationOn size={25} className=' text-white' />
+                            </div>
+                            <p className='text-slate-400'>Uganda - Kampala</p>
                         </div>
                     </div>
+
+                    <div className=' ml-10 '>
+                        <input type='text' placeholder='Name' className=' text-white border border-zinc-900 pl-2 mb-3 block w-96 h-10 bg-black' />
+                        <input type='email' placeholder='Email' className=' pl-2 mb-3 block w-96 h-10 border border-zinc-900 bg-black' />
+                        <textarea placeholder='Message' className=' pl-2 mb-3 block w-96 h-32 resize-none border border-zinc-900 bg-black'/>
+                        <button className=' text-white bg-rose-600 px-4 py-4 mt-4 hover:bg-black border-2 border-rose-600'>Send Message</button>
+                    </div>
+
                 </div>
             </div>
         </section>
